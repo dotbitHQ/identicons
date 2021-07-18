a986b4b107fe823fed98aacdc867c81ec1e6e492
longhuicode <longhuicode@foxmail.com>
2021-07-18 20:17:25 +0800

    fix: add Header cache-control

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,6 +8,7 @@ export class AppController {
   @Get('/identicon/:name')
   @Header('content-type', 'image/png')
   @Header('accept-ranges', 'bytes')
+  @Header('Cache-Control', 'public, max-age=' + 30 * 24 * 60 * 60)
   async identicon(@Res() res, @Param('name') name) {
     const _identiconBuffer = await this.appService.identicon(
       name.toLocaleLowerCase()
