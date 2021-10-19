7871cc877994d6d10520fbe48b289527c87506d6
jeffjing <zgayjjf@qq.com>
2021-10-19 18:15:50 +0800

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import config from '../config'
 
-async function bootstrap() {
+async function bootstrap (): Promise<void> {
   const app = await NestFactory.create(AppModule)
+
+  app.enableCors()
+
   await app.listen(config.port)
 }
-bootstrap()
+
+void bootstrap()
